2de870cc610bb14eba9f208874fa2ac4c3c51c77
krrehman <k.rehman77@gmail.com>
2023-07-25 09:39:01 +1000

    Changed envConfig to use bom url as default

--- tests/src/common/envConfig.ts
+++ tests/src/common/envConfig.ts
@@ -1,17 +1,17 @@
 interface ITestData {
-  BASE_URL: string
+  BASE_URL: string;
 }
 
 export type Env = 'development' | 'bomLive';
 
-const generateConfig = (urlOverride = 'https://localhost:3000') => {
+const generateConfig = (urlOverride = 'http://www.bom.gov.au') => {
   const BASE_URL = process.env.URL || urlOverride;
-  return {BASE_URL};
+  return { BASE_URL };
 };
 
 const testData: Record<Env, ITestData> = {
   development: generateConfig(),
-  bomLive:generateConfig('http://www.bom.gov.au')
+  bomLive: generateConfig('http://www.bom.gov.au')
 };
 
 export const formTestData: ITestData = testData[process.env?.REACT_APP_STAGE ?? 'development'];
